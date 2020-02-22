7777e07a0b19531f6a6b9038f7ca830000d48325
silascaimi <silascaimi@live.com>
2020-02-21 22:09:59 -0300

    Inserir aplicação de entrada de dados

---
+++ data-input/src/br/com/input/InputTeste.java
@@ -0,0 +1,26 @@
+package br.com.input;
+
+import java.util.Scanner;
+
+public class InputTeste {
+
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+
+		System.out.print("Nome: ");
+		String nome = scanner.nextLine();
+
+		System.out.println("Idade: ");
+		int idade = scanner.nextInt();
+
+		System.out.println("Peso: ");
+		double peso = scanner.nextDouble();
+
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Peso: " + peso);
+	}
+
+}
