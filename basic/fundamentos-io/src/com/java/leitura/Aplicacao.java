9b25c823c32e3c2eae9d7b44fe70f21d5d9e23f8
silascaimi <silascaimi@live.com>
2020-04-06 14:29:31 -0300

    Atualizar aplicação

--- basic/fundamentos-io/src/com/java/leitura/Aplicacao.java
+++ basic/fundamentos-io/src/com/java/leitura/Aplicacao.java
@@ -1,14 +1,14 @@
 package com.java.leitura;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Aplicacao {
 
-	public static void main(String[] args) throws IOException {
-		/*// Método 1
-		 * try (InputStream is = new FileInputStream("entrada.txt")) {
+	public static void main(String[] args) {
+		/*
+		 * // Método 1 try (InputStream is = new FileInputStream("entrada.txt")) {
 		 * 
 		 * byte[] buffer = new byte[1024];
 		 * 
@@ -18,46 +18,41 @@ public class Aplicacao {
 		 * bytesLidos para evitar lixo System.out.println(texto); }
 		 * 
 		 */
-		
-		
-		/*// Método 2
-		 * // Maneira de criação dinâmica alocando apenas o espaço necessário try
-		 * (InputStream is = new FileInputStream("entrada.txt")) {
+
+		/*
+		 * // Método 2 // Maneira de criação dinâmica alocando apenas o espaço
+		 * necessário try (InputStream is = new FileInputStream("entrada.txt")) {
 		 * 
 		 * byte[] buffer = new byte[4];
 		 * 
-		 * StringBuilder texto = new StringBuilder();
-		 * int bytesLidos;
+		 * StringBuilder texto = new StringBuilder(); int bytesLidos;
 		 * 
 		 * while((bytesLidos = is.read(buffer)) > -1) { texto.append(new String(buffer,
 		 * 0, bytesLidos)); }
 		 * 
 		 * System.out.println(texto); }
 		 */
-		
-		
 
-		/* // Método 3 
-		 * // Maneira mais simples de se trabalhar try(BufferedReader reader
+		/*
+		 * // Método 3 // Maneira mais simples de se trabalhar try(BufferedReader reader
 		 * = new BufferedReader(new FileReader("entrada.txt"))){
 		 * 
-		 * String texto = "";
-		 * String line;
-		 * while ((line = reader.readLine()) != null) {
+		 * String texto = ""; String line; while ((line = reader.readLine()) != null) {
 		 * texto += line + "\n"; }
 		 * 
 		 * System.out.println(texto); }
 		 */
-		
-		
+
 		// Método mais simplificado
-		try(Scanner scanner = new Scanner(new File("entrada.txt"))){
+		try (Scanner scanner = new Scanner(new File("entrada.txt"))) {
 			String s = "";
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
 				s += line + "\n";
 			}
 			System.out.println(s);
+		} catch (FileNotFoundException e) {
+			e.getMessage();
 		}
 
 	}
