2fa7bee70e8ae7aab05877ed2e603430c00b6665
silascaimi <silascaimi@live.com>
2020-04-16 12:19:40 -0300

    Utilizar formatação de números

---
+++ basic/numeros/src/com/java/testeBigDecimal.java
@@ -0,0 +1,22 @@
+package com.java;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class testeBigDecimal {
+	
+	static Scanner entrada = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.println("Informe o valor do produto: ");
+		double valor = entrada.nextDouble();
+		System.out.println(valor);
+		
+		DecimalFormat df = new DecimalFormat("R$ #,##0.00");
+		
+		BigDecimal bd = new BigDecimal(valor);
+		bd = bd.divide(BigDecimal.TEN); // Captura a d�cima parte do n�mero
+		System.out.println(df.format(bd.doubleValue()));
+	}
+}
